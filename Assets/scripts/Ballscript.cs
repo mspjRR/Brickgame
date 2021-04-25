613d248ce47b8591f9b7986bc7c69cbe7d1398f4
MSPJ\ramar <ramarao714@gmail.com>
2021-04-25 14:34:39 +0530

    brick game testrun

---
+++ Assets/scripts/Ballscript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballscript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inplay;
+    public Transform paddle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inplay)
+        {
+            transform.position = paddle.position;
+        }
+        if (Input.GetButtonDown ("Jump") && !inplay)
+        {
+            inplay = true;
+            rb.AddForce(Vector2.up * 500);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("bottom"))
+        {
+           
+            rb.velocity = Vector2.zero;
+            inplay = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("brick"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/Paddlescript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddlescript : MonoBehaviour
+{
+    public float speed;
+    public float rightscreenedge;
+    public float leftscreenedge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* float mousepos = Input.mousePosition.x / Screen.width * 16.0f;
+         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+         paddlePos.x = Mathf.Clamp(mousepos, 0, 11);
+         transform.position = paddlePos;*/
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontal*Time.deltaTime*speed);
+        if(transform.position.x < leftscreenedge)
+        {
+           transform.position = new Vector2 (leftscreenedge, transform.position.y);
+        }
+        if (transform.position.x > rightscreenedge)
+        {
+            transform.position = new Vector2 (rightscreenedge, transform.position.y);
+        }
+
+    }
+}
